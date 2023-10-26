15a823669939808db30d456633f4a482a7ef13ed
Artem Bykov <bykovaa94@mail.ru>
2023-10-26 17:23:32 +0300

    clian code

--- src/components/movingBlock/Block.js
+++ src/components/movingBlock/Block.js
@@ -2,34 +2,34 @@ import React, { useEffect, useRef, useState } from "react";
 import '../css/Block.css';
 
 const Block = () => {
-    const [draggingsMouse, setDraggingMouse] = useState({ x: 0, y: 0 })
-    const [coordinatesBlock, setCoordinatesBlock] = useState(false)
+    const [draggingsMouse, setDraggingMouse] = useState(false)
+    const [coordinatesBlock, setCoordinatesBlock] = useState({ x: 0, y: 0 })
     const ref = useRef(null)
 
     const hendleMouse = (e) => {
-        if(coordinatesBlock) {
-            setDraggingMouse({ x : draggingsMouse.x + e.movementX, y : draggingsMouse.y + e.movementY})
+        if(draggingsMouse) {
+            setCoordinatesBlock({ x : coordinatesBlock.x + e.movementX, y : coordinatesBlock.y + e.movementY})
         }
     }
         useEffect(() => {
             if (ref.current) {
-              ref.current.style.left = `${draggingsMouse.x}px`
-              ref.current.style.top = `${draggingsMouse.y}px`
+              ref.current.style.left = `${coordinatesBlock.x}px`
+              ref.current.style.top = `${coordinatesBlock.y}px`
             }
-          }, [draggingsMouse])
+          }, [coordinatesBlock])
 
-          
+
     return (
         <div
             className='block_warper'
             onMouseMove={(e) => hendleMouse(e)}
-            onMouseUp={() => setCoordinatesBlock(false)}
+            onMouseUp={() => setDraggingMouse(false)}
             >    
             <div
                 className='block'
                 ref={ref}
-                onMouseDown={() => setCoordinatesBlock(true)}
-                onMouseUp={() => setCoordinatesBlock(false)}
+                onMouseDown={() => setDraggingMouse(true)}
+                onMouseUp={() => setDraggingMouse(false)}
             >
             </div>
         </div>
